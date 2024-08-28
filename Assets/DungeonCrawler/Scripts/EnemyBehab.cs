73e4afbaeb21335bc91ffe339aa5efdc6c43937c
CocoHoshi <Carlaz020400@gmail.com>
2024-08-28 12:59:25 -0600

    NewMovement And StateMachine

--- Assets/DungeonCrawler/Scripts/EnemyBehab.cs
+++ Assets/DungeonCrawler/Scripts/EnemyBehab.cs
@@ -6,9 +6,16 @@ namespace TPGG.FiniteStateMachine {
 
     public enum States {
 
-        IDLE,
-        PATROLLING,
-        PERSECUTING
+        //IDLE
+        IDLE_UP,
+        IDLE_DOWN,
+        IDLE_LEFT,
+        IDLE_RIGHT,
+        //MOVING
+        MOVING_UP,
+        MOVING_DOWN,
+        MOVING_LEFT,
+        MOVING_RIGHT
     }
     public class EnemyBehab : MonoBehaviour {
 
@@ -20,29 +27,31 @@ namespace TPGG.FiniteStateMachine {
         void Start() {
             speed = 5;
             //instation of the variable
-            myState = States.IDLE;
+            myState = States.IDLE_DOWN;
         }
         //operation of the variable
         void Update() {
+
+            Debug.Log(gameObject.name + " Finite State Machine - Update() - is in any IDLE state " + isInIdleState);
 
             Debug.Log(gameObject.name + " Finite State Machine - Update() - enemy is in " + myState.ToString());
 
             switch (myState) {
 
-                case States.IDLE:
-                    //Code
+                case States.IDLE_UP:
+                case States.IDLE_DOWN:
+                case States.IDLE_RIGHT:
+                case States.IDLE_LEFT:
+                    IdleState();
+                    break;
+                case States.MOVING_UP:
+                case States.MOVING_DOWN:
+                case States.MOVING_RIGHT:
+                case States.MOVING_LEFT:
                     IdleState();
                     break;
 
-                case States.PATROLLING:
-                    //Code
-                    PatrollingState();
-                    break;
 
-                case States.PERSECUTING:
-                    //Code
-                    PersecutingState();
-                    break;
             }
         }
 
@@ -50,35 +59,48 @@ namespace TPGG.FiniteStateMachine {
 
 
         protected void IdleState() {
+           
+
 
         }
-        protected void PatrollingState() {
+        protected void MovingState() {
 
         }
         protected void PersecutingState() {
 
-            Invoke("ChangeToIdleState",3.0f);
+            //Invoke("ChangeToIdleState",3.0f);
         }
 
         protected void ChangeToIdleState() {
-            ChangeState(States.IDLE);
+            //hangeState(States.IDLE);
         }
-        public void ChangeState(States value) {
-
-            Debug.Log(gameObject.name + " Finite State Machine - Update() - state is going to change from " + myState.ToString() + " to " + value.ToString());
-            myState = value;
-            Debug.Log(gameObject.name + " Finite State Machine - Update() - state is now " + myState.ToString());
-        }
+        
 
         #endregion
         void FixedUpdate() {
-            float dt = Time.deltaTime;
-
-            Vector3 movement = new Vector3(0, 0, 0);
-
-            movement += new Vector3((Input.GetAxis("Horizontal") * speed * dt), (Input.GetAxis("Vertical") * speed * dt), 0);
-
-            transform.position += movement;
+          
         }
+
+        #region GetSets
+
+        protected bool isInIdleState {
+
+            get{ 
+            return (myState == States.IDLE_UP || myState == States.IDLE_DOWN || myState == States.IDLE_RIGHT || myState == States.IDLE_LEFT);
+            }
+            
+        }
+
+        public States SetState {
+            set {
+                Debug.Log(gameObject.name + " Finite State Machine - Update() - state is going to change from " + myState.ToString() + " to " + value.ToString());
+                myState = value;
+                Debug.Log(gameObject.name + " Finite State Machine - Update() - state is now " + myState.ToString());
+            }
+        }
+
+
+
+        #endregion GetSets
     }
 }
